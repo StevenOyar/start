32ad154debf88922df640e165da7ae153b7c9e50
Steven Oyaro <stevenoyaro@Stevens-MacBook-Air.local>
2023-05-11 19:13:06 +0300

    removed comments kanye.py

--- kanye.py
+++ kanye.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 
 def get_quote():
-    # Write your code here.
+    
     response = requests.get(url="https://api.kanye.rest")
     response.raise_for_status()
     data = response.json()
@@ -16,13 +16,12 @@ window.title("Kanye Says...")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=300, height=414)
-# background_img = PhotoImage(file ="background.png")
-# canvas.create_image(150, 207, image=background_img)
+
 quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 24, "bold")
                                 , fill="blue")
 canvas.grid(row=0, column=0)
 
-# kanye_img = PhotoImage(file="kanye.png")
+
 kanye_button = Button(highlightthickness=0, command=get_quote, text="Kanye Once said", fg="brown", bg="teal")
 kanye_button.grid(row=1, column=0, columnspan=2)
 
